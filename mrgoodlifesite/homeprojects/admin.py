fddb8eee65399ed68268ff3085bb924e9d4835a2
Ryan Bonavida <rbonavida@sram.com>
2022-02-14 09:41:00 -0700

    Update admin page for Task to be usable

--- mrgoodlifesite/homeprojects/admin.py
+++ mrgoodlifesite/homeprojects/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-from .models import Task
-admin.site.register(Task)
+from .models import Task, Tool, Step
+
+
+class StepInLine(admin.TabularInline):
+    model = Step
+    extra = 3
+
+
+class ToolInLine(admin.TabularInline):
+    model = Tool
+    extra = 3
+
+
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [ToolInLine, StepInLine]
+    list_display = ['name', 'priority', 'due_date']
+
+admin.site.register(Task, TaskAdmin)
