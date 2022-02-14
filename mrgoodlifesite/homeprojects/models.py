becfd59f583275154a9d09e765b6b637da29f2d7
Ryan Bonavida <rbonavida@sram.com>
2022-02-14 09:40:28 -0700

    Add steps model and set due_date, cadence to null by default

--- mrgoodlifesite/homeprojects/models.py
+++ mrgoodlifesite/homeprojects/models.py
@@ -8,15 +8,31 @@ class Task(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=1000, blank=True)
     priority = models.IntegerField(default=0)
-    due_date = models.DateTimeField(blank=True)
-    cadence = models.DurationField(blank=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+    cadence = models.DurationField(blank=True, null=True)
     active = models.BooleanField(default=True)
+    # steps foriegn key to Step
     # tool foreign key to Tool
     # journal foreign key to JournalEntry
 
+    def __str__(self):
+        return f"{self.name} - priority: {self.priority}"
+
 
 class Tool(models.Model):
-    task = models.ForeignKey('Task', related_name="tool", on_delete=models.CASCADE)
+    task = models.ForeignKey('Task', related_name="tools", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Step(models.Model):
+    task = models.ForeignKey('Task', related_name='steps', on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class JournalEntry(models.Model):
@@ -24,3 +40,5 @@ class JournalEntry(models.Model):
     creation_date = models.DateTimeField()
     task = models.ForeignKey('Task', related_name="journal", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"JournalEntry - {self.creation_date}"
