8411063ac7081f173a1c2f67707fc2a41d0d1046
Ryan Bonavida <ryan.bonavida@gmail.com>
2022-03-24 20:35:22 -0600

    Add view to show prioritized list of items

--- mrgoodlifesite/homeprojects/views.py
+++ mrgoodlifesite/homeprojects/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Task
+from django.template import loader
 
-# Create your views here.
+
+def index(request):
+    tasks = Task.objects.order_by("priority")
+    template = loader.get_template('homeprojects/index.html')
+    context = {
+        'prioritized_list': tasks
+    }
+    return HttpResponse(template.render(context, request))
+
